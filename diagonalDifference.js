2cbd1e2c7c65a0380a68db600b94d99eebb2dbae
unknown <rafalbiel77@gmail.com>
2020-11-18 19:46:03 +0100

    changed from fori to forEach

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,21 +1,16 @@
 let arr = [[1, 2, 3, 1], [4, 5, 6, 1], [7, 8, 9, 1], [1, 1, 1, 1]];
 
 function diagonalDifference(arr) {
-    let x = arr.length;
-    let sum = 0;
-    let sumL = 0;
-    let sumR = 0;
-    let y = x;
-    for (let i = 0; i < x; i++) {
-        console.log(arr[i][i]);
-        sumL = sumL + arr[i][i];
-        console.log(arr[i][y - 1]);
-        sumR = sumR + arr[i][y - 1];
-        y--;
-    }
-    console.log(sumL);
-    console.log(sumR);
-    sum = Math.abs(sumR - sumL);
-    return sum;
+    let difference = 0;
+    let sumLeftDiagonal = 0;
+    let sumRightDiagonal = 0;
+    let i = 0;
+
+    arr.forEach(function (element) {
+        sumRightDiagonal = sumRightDiagonal + element[i];
+        sumLeftDiagonal = sumLeftDiagonal + element[element.length - 1 - i];
+        i++;
+    });
+    return difference = Math.abs(sumRightDiagonal - sumLeftDiagonal);
 }
 console.log(diagonalDifference(arr));
